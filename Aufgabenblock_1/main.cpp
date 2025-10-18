6e09cfa86f90db67262664c4fcafc5bac238890c
Mikita Bianko <biankomikita@gmail.com>
2025-10-18 21:56:03 +0200

    init Aufgabenblock_1

---
+++ Aufgabenblock_1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Source.cpp
+ *
+ *  Created on: 18 Oct 2025
+ *      Author: mikitabianko
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello, World!";
+
+	return 0;
+}
